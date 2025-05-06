5e2c5946e02aa52c1e611e7baf5218a94ba63ff3
StrawberSam <s.roche53@hotmail.com>
2025-05-06 10:55:33 +0200

    :hammer: (2-print_alphabet.py) : fix the , at 2:18

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for i in range(97,123):
+for i in range(97, 123):
     print("{}".format(chr(i)), end="")
 # Affiche l'alphabet ASCII en minuscules sans retour à la ligne
 # chr(i) prend un nombre entier et le convertit à son code ASCII
