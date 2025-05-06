03154ffb5c5d2c4d8d211cf8c0ba5a2465390f68
StrawberSam <s.roche53@hotmail.com>
2025-05-06 10:54:31 +0200

    :sparkles: (2-print_alphabet.py) : add the file for the task 2

---
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+for i in range(97,123):
+    print("{}".format(chr(i)), end="")
+# Affiche l'alphabet ASCII en minuscules sans retour à la ligne
+# chr(i) prend un nombre entier et le convertit à son code ASCII
+# "{}".format() insère dynamiquement une valeur dans une chaîne de caractères
