4f58311eea9222ea058071fdc25146b657b5067c
StrawberSam <s.roche53@hotmail.com>
2025-05-07 10:59:23 +0200

    :hammer: (9-print_last_digit.py) : Fix the space before the comment for pycodestyle

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    last_digit = abs(number) % 10 # Transforme le nbr en val absolue
+    last_digit = abs(number) % 10  # Transforme le nbr en val absolue
     print("{}".format(last_digit), end="")
     return last_digit
 # Abs sert à transformer le nbr en valeur absolue(pour nombre négatif)
