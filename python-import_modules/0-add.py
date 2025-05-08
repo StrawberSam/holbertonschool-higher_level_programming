deeda407e42e76d2e4811a0867c13c045b467f52
StrawberSam <s.roche53@hotmail.com>
2025-05-08 10:52:09 +0200

    :speech_balloon: (0-add.py) : Added comment for what I didn't understand

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from add_0 import add
+if __name__ == "__main__":  # Protection d'execution
+    from add_0 import add  # Importation de la fonction add depuis add_0
     a = 1
     b = 2
     print("{} + {} = {}".format(a, b, add(a, b)))
