384a28dc725f26126bd96c725dcb431e14789542
StrawberSam <s.roche53@hotmail.com>
2025-05-14 19:41:08 +0200

    🔨 (12-roman_to_int.py) : for pycodestyle

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -3,8 +3,10 @@ def roman_to_int(roman_string):
     if not isinstance(roman_string, str):
         return (0)
 
-    dictionary = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
-                'C': 100, 'D': 500, 'M': 1000}
+    dictionary = {
+        'I': 1, 'V': 5, 'X': 10, 'L': 50,
+        'C': 100, 'D': 500, 'M': 1000
+        }
 
     total = 0
     i = 0
@@ -12,8 +14,8 @@ def roman_to_int(roman_string):
     while i < len(roman_string):
         if (
             i + 1 < len(roman_string)
-        and dictionary[roman_string[i]] < dictionary[roman_string[i + 1]]
-        ):
+            and dictionary[roman_string[i]] < dictionary[roman_string[i + 1]]
+           ):
             total += (
                 dictionary[roman_string[i + 1]] -
                 dictionary[roman_string[i]]
