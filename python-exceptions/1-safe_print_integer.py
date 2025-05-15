b2705c1dd81a541cb1453db8485cf65b3d829326
StrawberSam <s.roche53@hotmail.com>
2025-05-15 13:48:34 +0200

    🔨  (1-safe_print_integer.py) : prints an integer with "{:d}".format().

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -5,5 +5,5 @@ def safe_print_integer(value):
         print("{:d}".format(value), end="")
         print()
         return (True)
-    except ValueError:
+    except (ValueError, TypeError):
         return (False)
