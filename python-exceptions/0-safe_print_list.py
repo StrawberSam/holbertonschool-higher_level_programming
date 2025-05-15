dcebfb61ad9c1787fbe1174dc8c2106038f40d27
StrawberSam <s.roche53@hotmail.com>
2025-05-15 13:25:41 +0200

    🔨  (0-safe_print_list.py) : chmod

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,10 +3,10 @@ def safe_print_list(my_list=[], x=0):
     compteur = 0
 
     for i in range(x):
-        try :
+        try:
             print(my_list[i], end="")
             compteur += 1
-        except :
+        except IndexError:
             break
     print()
     return (compteur)
