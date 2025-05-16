8cf6d7e2063c324f7cd5bc8988329020e60f9197
StrawberSam <s.roche53@hotmail.com>
2025-05-16 11:08:02 +0200

    ✨ (0-add_integer.py) : adds 2 integers.

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+    Add two integers or floats together
+    and return the result as an integer.
+"""
+
+def add_integer(a, b=98):
+    """_summary_
+
+    Args:
+        a (_type_): variable
+        b (int, optional): variable, defaults to 98.
+
+    Raises:
+        TypeError: a doit être un entier
+        TypeError: b doit être un entier
+
+    Returns:
+        _type_: retourne un int
+    """
+
+
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    return (int(a) + int(b))
