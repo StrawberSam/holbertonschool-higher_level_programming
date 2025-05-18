44bf552f7dc1f5f9171ac58aa7c5e0d60371ad5d
StrawberSam <s.roche53@hotmail.com>
2025-05-18 14:34:17 +0200

    🔨 📝 🪂(5-text_indentation.txt and .py) : fix for pycodestyle and fix code. Add documentation.

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,5 +1,19 @@
 #!/usr/bin/python3
+"""
+    prints a text with 2 new lines after each of these characters: ., ? and :
+"""
+
+
 def text_indentation(text):
+    """
+    prints a text with 2 new lines after each of these characters: ., ? and :
+
+    Args:
+        text (string): contient un texte
+
+    Raises:
+        TypeError: le texte doit être de type string
+    """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
