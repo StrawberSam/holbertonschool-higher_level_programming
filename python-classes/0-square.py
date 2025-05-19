de09ed5fea21e65c30f8d9b7a9a1b2a871c3a323
StrawberSam <s.roche53@hotmail.com>
2025-05-19 10:39:34 +0200

    ✨ (0-square.py) : the file for the project,  write an empty class 'Square' that defines a square.

---
+++ python-classes/0-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Ce module contient la définition de la classe Square.
+Il sert de base pour représenter un carré dans le contexte POO
+"""
+
+
+class Square:
+    """
+    Classe représentant un carré.
+    Elle contiendra des attributs.
+    """
+    pass
