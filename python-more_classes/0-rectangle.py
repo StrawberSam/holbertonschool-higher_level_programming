094d3cce4f283a493c644d088016fe97b097e433
StrawberSam <s.roche53@hotmail.com>
2025-05-20 11:09:33 +0200

    ✨(0-rectangle.py) : write a empty class Rectangle that defines a rectangle.

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Ce module contient la définition de la classe Rectangle.
+Il sert de base pour représenter un rectangle dans le contexte POO
+"""
+
+
+class Rectangle:
+    """
+    Classe représentant un rectangle.
+    Elle contiendra des attributs.
+    """
+    pass
