0847756f6d459bca76f6c1d862b5518f87e59ccb
StrawberSam <s.roche53@hotmail.com>
2025-05-26 15:30:31 +0200

    ✨(4-inherits_from) : returns True if the object is an instance of a class that inherited (directly or indirectly) from the specified class ; otherwise False.

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -19,10 +19,9 @@ def inherits_from(obj, a_class):
     Returns:
         True/false: Vrai ou faux
     """
-    if type(obj) == a_class:
+    if type(obj) is a_class:
         return False
     elif isinstance(obj, a_class):
         return True
     else:
         return False
-
