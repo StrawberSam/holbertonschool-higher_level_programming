c8bc33a27a1b31b94fa695dadefeb6220dcb3cee
StrawberSam <s.roche53@hotmail.com>
2025-05-26 15:33:50 +0200

    ✨(5-base_geometry) : Write an empty class BaseGeometry.

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module contenant la classe BaseGeometry.
+Cette classe servira de base pour définir des formes géométriques.
+"""
+
+
+class BaseGeometry:
+    """Classe de base pour les opérations géométriques.
+
+    Cette classe est destinée à être étendue par d'autres classes
+    représentant des formes géométriques spécifiques.
+    """
+    pass
