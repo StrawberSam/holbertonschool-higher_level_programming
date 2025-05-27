b9a3e507c9cc7f428ac294ad32cb6290be2bb707
StrawberSam <s.roche53@hotmail.com>
2025-05-27 14:31:53 +0200

    ✨(11-square) : str for return the description of the Square

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+"""Import contenant la classe Rectangle.
+Cette classe servira de base pour définir des formes géométriques.
+"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Module contenant la classe Square, héritant de Rectangle.
+    Cette classe servira de base pour définir des formes géométriques.
+    """
+
+    def __init__(self, size):
+        """conducteur avec attributs d'instance
+
+        Args:
+            size (int) : taille
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(self.__size, self.__size)
+
+    def area(self):
+        """définit l'aire du carré
+
+        Returns:
+            int: l'aire du carré
+        """
+        squarearea = self.__size * self.__size
+        return squarearea
+
+    def __str__(self):
+        """retourne la description du rectangle
+
+        Returns:
+            return en f-string
+        """
+        return f"[Square] {self._width}/{self.__height}"
