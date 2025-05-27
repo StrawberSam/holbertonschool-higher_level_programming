5f73c70d2a4cf4085d89d935b93d335a9d31d656
StrawberSam <s.roche53@hotmail.com>
2025-05-27 10:03:10 +0200

    ✨(7-base_geometry) : Public instance method: def integer_validator(self, name, value): that validates value: you can assume name is always a string if value is not an integer: raise a TypeError exception, with the message <name> must be an integer if value is less or equal to 0: raise a ValueError exception with the message <name> must be greater than 0

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+"""Module contenant la classe BaseGeometry.
+Cette classe servira de base pour définir des formes géométriques.
+"""
+
+
+class BaseGeometry:
+    """Classe de base pour les opérations géométriques.
+
+    Cette classe est destinée à être étendue par d'autres classes
+    représentant des formes géométriques spécifiques.
+    """
+    def area(self):
+        """
+        Calcule l'aire de la forme géométrique.
+
+        Raises:
+            Exception: Indique que la méthode n'est pas implémentée.
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Public instance method: def integer_validator(self, name, value):
+        that validates value:
+        you can assume name is always a string
+        if value is not an integer: raise a TypeError exception,
+        with the message <name> must be an integer
+        if value is less or equal to 0: raise a ValueError exception
+        with the message <name> must be greater than 0
+
+        Args:
+            name (_type_): nom
+            value (_type_): valeur
+
+        Raises:
+            TypeError: si le type est pas le bon
+            ValueError: si la valeur n'est pas bonne
+        """
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
