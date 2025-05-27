2ff969bb6196ce565a2a0c5683438612661e0f9e
StrawberSam <s.roche53@hotmail.com>
2025-05-27 10:11:03 +0200

    🔨 (7-base_geometry) : Changement condition

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -38,7 +38,7 @@ class BaseGeometry:
             TypeError: si le type est pas le bon
             ValueError: si la valeur n'est pas bonne
         """
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
