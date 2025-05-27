d8cbca87aaf69bba0e5acf81708d7b639361536d
StrawberSam <s.roche53@hotmail.com>
2025-05-27 14:01:22 +0200

    📝🔨 (9-rectangle) : docstring

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -15,14 +15,30 @@ class Rectangle(BaseGeometry):
     """
 
     def __init__(self, width, height):
+        """conducteur avec attributs d'instance
+
+        Args:
+            width (int): largeur
+            height (int): hauteur
+        """
         self.integer_validator("width", width)
         self.__width = width
         self.integer_validator("height", height)
         self.__height = height
 
     def area(self):
+        """définit l'aire du rectangle
+
+        Returns:
+            int: l'aire du rectangle
+        """
         recarea = self.__width * self.__height
         return recarea
 
     def __str__(self):
+        """retourne la description du rectangle
+
+        Returns:
+            return en f-string
+        """
         return f"[Rectangle] {self.__width}/{self.__height}"
