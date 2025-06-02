ccfc53caa02a9ea4e72558fa0e7e4e3ac49e3189
StrawberSam <s.roche53@hotmail.com>
2025-06-02 14:51:44 +0200

    ✨ (0-read_file) : function that reads a text file (UTF-8) and prints it to stdout

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Module read_file
+
+Ce module contient une fonction qui lit un fichier texte UTF-8
+et affiche son contenu sur la sortie standard.
+"""
+
+def read_file(filename=""):
+    """
+    Lit un fichier texte en UTF-8 et affiche son contenu à l'écran.
+
+    Args:
+        filename (str): Le chemin du fichier à lire. Doit être une chaîne de caractères.
+                        Par défaut, une chaîne vide.
+
+    Returns:
+        None
+
+    Ce n'est pas une fonction de lecture silencieuse :
+    le contenu est affiché avec un print().
+    """
+    with open(filename, "r", encoding="utf-8") as file:
+        contenu = file.read()
+        print(contenu, end="")
