9b5eef505f752733000ce63a517881de2aed8749
StrawberSam <s.roche53@hotmail.com>
2025-06-04 20:09:01 +0200

    🔨  (5-save_to_json_file) : pycodestyle, line too long

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-Ce module fournit une fonction pour enregistrer un objet Python dans un fichier texte,
-en utilisant une représentation JSON.
+Ce module fournit une fonction pour enregistrer un objet
+Python dans un fichier texte, en utilisant une représentation JSON.
 """
 
 import json
@@ -16,9 +16,10 @@ def save_to_json_file(my_obj, filename):
         filename (str) : Le nom du fichier dans lequel enregistrer l'objet.
 
     Remarques :
-        - La fonction n’effectue aucune gestion des exceptions si l’objet ne peut pas être
-          sérialisé en JSON.
-        - Elle n’effectue pas non plus de gestion des erreurs liées aux permissions de fichiers.
+        - La fonction n’effectue aucune gestion des exceptions si
+        l’objet ne peut pas être sérialisé en JSON.
+        Elle n’effectue pas non plus de gestion des erreurs liées aux
+        permissions de fichiers.
     """
     json_file = json.dumps(my_obj)
     with open(filename, "w", encoding="utf-8") as file:
