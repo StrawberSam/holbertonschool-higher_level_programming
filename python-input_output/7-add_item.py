a34e14958f6b3798bda8a0bd1562e3b331e90324
StrawberSam <s.roche53@hotmail.com>
2025-06-05 13:37:05 +0200

    🔨  (7-add_item) : delete the import json

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -13,7 +13,6 @@ Tous les arguments passés au script sont ajoutés à la liste existante.
 """
 
 import sys
-import json
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
