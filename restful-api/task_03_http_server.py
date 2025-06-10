c9cd530eee72a6ba177fc4a9f0d64eb74f08e7a0
StrawberSam <s.roche53@hotmail.com>
2025-06-10 14:30:54 +0200

    🔨 (task_03_http_server) : add end_headers

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -76,6 +76,7 @@ class HandlerRequests(http.server.BaseHTTPRequestHandler):
         else:
             self.send_response(404)
             self.send_header("Content-Type", "text/plain")
+            self.end_headers()
             self.wfile.write(b"Endpoint not found")
 
 
