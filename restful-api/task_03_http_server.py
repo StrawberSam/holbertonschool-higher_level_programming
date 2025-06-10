d2b97b6d854290c237e551d820cf4670f8558454
StrawberSam <s.roche53@hotmail.com>
2025-06-10 14:30:12 +0200

    🔨 (task_03_http_server) : ligne 78 : text/plain

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -75,7 +75,7 @@ class HandlerRequests(http.server.BaseHTTPRequestHandler):
 
         else:
             self.send_response(404)
-            self.send_header("Content-Type", "application/json")
+            self.send_header("Content-Type", "text/plain")
             self.wfile.write(b"Endpoint not found")
 
 
