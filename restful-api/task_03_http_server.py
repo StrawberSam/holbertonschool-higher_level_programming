b46dce0d685406cdc8c02a1040443a74dd616002
StrawberSam <s.roche53@hotmail.com>
2025-06-10 13:42:57 +0200

    ✨ (task_03_http_server) : Set up a basic web server using the http.server module. Handle different types of HTTP requests (GET, POST, etc.). Serve JSON data in response to specific endpoints.

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+import http.server
+import json
+
+
+class HandlerRequests(http.server.BaseHTTPRequestHandler):
+    """
+    Gère les requêtes HTTP entrantes pour un serveur web simple.
+
+    Cette classe hérite de BaseHTTPRequestHandler et
+    implémente la méthode do_GET
+    pour répondre aux requêtes GET sur des chemins spécifiques.
+    """
+
+    def do_GET(self):
+        """
+        Gère les requêtes HTTP GET.
+
+        Cette méthode analyse le chemin de la requête (self.path)
+        et envoie une réponse JSON appropriée en fonction du chemin :
+        - Si le chemin est "/data", retourne des info sur un utilisateur.
+        - Si le chemin est "/info", retourne des informations sur l'API.
+        - Pour tout autre chemin, retourne une erreur 404 (Not Found).
+        """
+        if self.path == "/data":
+            self.send_response(200)
+            self.send_header("Content-Type", "application/json")
+            self.end_headers()
+
+            data = {"name": "John", "age": 30, "city": "New York"}
+            json_data = json.dumps(data)
+            self.wfile.write(json_data.encode('utf-8'))
+
+        elif self.path == "/info":
+            self.send_response(200)
+            self.send_header("Content-Type", "application/json")
+            self.end_headers()
+
+            datas = {
+                "version": "1.0",
+                "description": "A simple API built with http.server"
+                }
+            json_data = json.dumps(datas)
+            self.wfile.write(json_data.encode('utf-8'))
+
+        elif self.path != "/data" and self.path != "/info":
+            self.send_response(404)
+            self.send_header("Content-Type", "application/json")
+            self.end_headers()
+
+            response = {"error": "Not Found"}
+            json_response = json.dumps(response)
+            self.wfile.write(json_response.encode('utf-8'))
+
+
+if __name__ == "__main__":
+    """
+    Point d'entrée principal du script.
+
+    Initialise et démarre un serveur HTTP sur le port 8080.
+    Le serveur utilise HandlerRequests pour gérer toutes les requêtes
+    entrantes. Le serveur tourne indéfiniment jusqu'à ce qu'il soit arrêté
+    manuellement.
+    """
+    server_address = ('', 8080)
+    httpd = http.server.HTTPServer(server_address, HandlerRequests)
+    httpd.serve_forever()
