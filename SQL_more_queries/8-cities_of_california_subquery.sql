12fdc8c08789116efce833b3f54de2783cce4483
StrawberSam <s.roche53@hotmail.com>
2025-06-25 10:57:04 +0200

    :hammer: (cities_of_california_subquery) | add USE for the DB

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -2,6 +2,8 @@
 -- The states table contains only one record where name = California (but the id can be different, as per the example)
 -- Results must be sorted in ascending order by cities.id
 -- You are not allowed to use the JOIN keyword
+USE hbtn_0d_usa;
+
 SELECT *
 FROM cities
 WHERE state_id = (
