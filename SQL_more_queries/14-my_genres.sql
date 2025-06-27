8b60e92ec5497ad0d37720903b3e41311fc61489
StrawberSam <s.roche53@hotmail.com>
2025-06-27 10:05:53 +0200

    :sparkles: (my_genres) |

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -3,7 +3,6 @@
 -- Each record should display: tv_genres.name
 -- Results must be sorted in ascending order by the genre name
 -- You can use only one SELECT statement
--- The database name will be passed as an argument of the mysql command
 SELECT tv_genres.name
 FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
