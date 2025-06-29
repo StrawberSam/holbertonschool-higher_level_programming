4f73ed90ee74c830014bec1abda32e112ef14498
StrawberSam <s.roche53@hotmail.com>
2025-06-29 09:48:54 +0200

    :hammer: (my_filter_states) | pycodestyle

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -24,7 +24,10 @@ if __name__ == "__main__":
     cursor = db.cursor()
 
     # Construction et exécution de la requête SQL
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(state_name)
+    query = (
+        "SELECT * FROM states WHERE name = '{}' "
+        "ORDER BY id ASC".format(state_name)
+    )
     cursor.execute(query)
 
     # Récupération et affichage des résultats
