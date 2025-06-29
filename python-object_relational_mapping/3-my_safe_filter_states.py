5247bdcc9a061adf29f0638962618a3d5acf64dd
StrawberSam <s.roche53@hotmail.com>
2025-06-29 09:54:00 +0200

    :sparkles: (my_safe_filter_states) |

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Safe script to filter states by name, protected from SQL injection."""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    # Récupération des arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    # Connexion à la base de données
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=db_name
+    )
+
+    cursor = db.cursor()
+
+    # Requête paramétrée pour se protéger contre les injections SQL : %s
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    cursor.execute(query, (state_name,))
+
+    results = cursor.fetchall()
+    for row in results:
+        print(row)
+
+    # Fermeture
+    cursor.close()
+    db.close()
