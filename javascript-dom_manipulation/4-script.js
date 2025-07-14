54a786aeae41c87977de1600ad053ea3e5fbed46
StrawberSam <s.roche53@hotmail.com>
2025-07-14 14:23:13 +0200

    🔨 | for semistandard, camelcase for add_item to addItem

--- javascript-dom_manipulation/4-script.js
+++ javascript-dom_manipulation/4-script.js
@@ -1,7 +1,7 @@
-const add_item = document.querySelector('#add_item');
+const addItem = document.querySelector('#add_item');
 const ul = document.querySelector('.my_list');
 
-add_item.addEventListener('click', () => {
+addItem.addEventListener('click', () => {
   const li = document.createElement('li');
   li.textContent = 'Item';
   ul.appendChild(li);
